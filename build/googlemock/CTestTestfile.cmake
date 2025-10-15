3b3de75a642cce5d0bbd074944afad6fe719888c
leekahwai <leekahwai@gmail.com>
2025-10-15 15:25:43 +0000

    remove build and update COVERAGE_AI_INSTRUCTIONS.md

--- build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspaces/CppMicroAgent/googletest-1.16.0/googlemock
-# Build directory: /workspaces/CppMicroAgent/build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspaces/CppMicroAgent/googletest-1.16.0/googletest
-# Build directory: /workspaces/CppMicroAgent/build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
